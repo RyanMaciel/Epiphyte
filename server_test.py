279b22efcc2f3f8f7ac075ec244750d24fe7411c
Ryan Maciel <rspams1234@gmail.com>
2022-03-12 15:23:09 -0500

    Added yolov5 code and used it to complete first vision to visualization system

--- server_test.py
+++ server_test.py
@@ -5,19 +5,30 @@ import time
 import random
 import websockets
 
+from vision import capture
+
 
 async def handler(websocket):
   print("handler called")
+
+  async def handle_position_data(pos):
+    if pos:
+      x = pos[0]
+      y = 50 #pos[1]
+      print(x)
+      print(y)
+      obj = {
+        "positions":[
+          {"x": x, "y":y}
+        ]
+      }
+      await websocket.send(json.dumps(obj))
+
+  
   while True:
-    obj = {
-      "positions":[
-        {"x": random.uniform(0, 100), "y":random.uniform(0, 100)}
-      ]
-    }
-    await websocket.send(json.dumps(obj))
-    time.sleep(0.5)
-  # async for message in websocket:
-  #     await websocket.send(message)
+    coords = capture()
+    print(coords)
+    await handle_position_data(coords)
 
 
 async def main():
